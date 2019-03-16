f5ef783851fc2a3cf944235b48ef540e9deff59d
Frank <frank.leuchtmann@gmx.de>
2019-03-16 16:05:03 +0100

    removed some doubles

--- src/Main.java
+++ src/Main.java
@@ -29,27 +29,29 @@ public class Main {
             conditonalsList.add(Integer.toString(i));
 
         }
-
+        System.out.println("length with 1 elements: " + conditonalsList.size());
 
     }
 
     private static void addTwoElements() {
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++)
-
-                conditonalsList.add(Integer.toString(i) + ", " + j);
+                if (i != j)
+                    conditonalsList.add(i + ", " + j);
 
         }
+        System.out.println("length with 2 elements: " + conditonalsList.size());
     }
 
     private static void addThreeElements() {
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++)
                 for (int k = 0; k < 4; k++)
-
-                    conditonalsList.add(Integer.toString(i) + ", " + j + ", " + k);
+                    if (i != j && i != k && j != k)
+                        conditonalsList.add(i + ", " + j + ", " + k);
 
         }
+        System.out.println("length with 3 elements: " + conditonalsList.size());
     }
 
     private static void addFourElements() {
@@ -57,9 +59,11 @@ public class Main {
             for (int j = 0; j < 4; j++)
                 for (int k = 0; k < 4; k++)
                     for (int l = 0; l < 4; l++)
-                        conditonalsList.add(Integer.toString(i) + ", " + j + ", " + k + ", " + l);
+                        if (i != j && i != k && i != l && j != k && j != l && k != l)
+                            conditonalsList.add(i + ", " + j + ", " + k + ", " + l);
 
         }
+        System.out.println("length with 4 elements: " + conditonalsList.size());
     }
 
 }
