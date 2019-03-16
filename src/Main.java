4d3c67d871cea2335f6b5cf613b796f5a7d477d2
Frank <frank.leuchtmann@gmx.de>
2019-03-16 16:47:37 +0100

    creates now lists until 2

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,15 @@ import java.util.List;
 
 public class Main {
 
-    private static List<List<String>> conditonalsList = new LinkedList<>();
+    private static List<List<Integer>> conditonalsList = new LinkedList<>();
 
     public static void main(String[] args) {
 
 
         addOneElements();
         addTwoElements();
-        addThreeElements();
-        addFourElements();
+        //addThreeElements();
+        //addFourElements();
 
         System.out.println("Number of elements: " + conditonalsList.size());
         printList(conditonalsList);
@@ -21,29 +21,36 @@ public class Main {
     }
 
 
-    private static void printList(List<List<String>> listList) {
-        for (List<String> stringFromList : listList)
+    private static void printList(List<List<Integer>> listList) {
+        for (List<Integer> stringFromList : listList)
             System.out.println(stringFromList);
     }
 
     private static void addOneElements() {
-        List<String> listToAdd = new LinkedList<>();
+
         for (int i = 0; i < 4; i++) {
-            listToAdd.add(Integer.toString(i));
+            List<Integer> listToAdd = new LinkedList<>();
+            listToAdd.add(i);
+            conditonalsList.add(listToAdd);
 
         }
-        conditonalsList.add(listToAdd);
         System.out.println("length with 1 elements: " + conditonalsList.size());
 
     }
 
     private static void addTwoElements() {
+
+
         for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++)
-                if (i != j)
-                    conditonalsList.add(i + ", " + j);
+            for (int j = 0; j < 4; j++) {
+                List<Integer> listToAdd = new LinkedList<>();
+                listToAdd.add(i);
+                listToAdd.add(j);
+                conditonalsList.add(listToAdd);
+            }
 
         }
+
         System.out.println("length with 2 elements: " + conditonalsList.size());
     }
 
@@ -52,7 +59,7 @@ public class Main {
             for (int j = 0; j < 4; j++)
                 for (int k = 0; k < 4; k++)
                     if (i != j && i != k && j != k)
-                        conditonalsList.add(i + ", " + j + ", " + k);
+                        System.out.println("lol");
 
         }
         System.out.println("length with 3 elements: " + conditonalsList.size());
@@ -64,10 +71,18 @@ public class Main {
                 for (int k = 0; k < 4; k++)
                     for (int l = 0; l < 4; l++)
                         if (i != j && i != k && i != l && j != k && j != l && k != l)
-                            conditonalsList.add(i + ", " + j + ", " + k + ", " + l);
-
+                            System.out.println("lol");
         }
         System.out.println("length with 4 elements: " + conditonalsList.size());
+    }
+
+    private void cleanList(){
+        removeSameValues();
+    }
+
+    private void removeSameValues(){
+        //for( List<Integer> intList : conditonalsList)
+            //if(intList.)
     }
 
 }
