a85f72a2bbeaf814f6e181b22ba32ec0cace5f75
Frank <frank.leuchtmann@gmx.de>
2019-03-16 15:27:45 +0100

    added two element lists

--- src/Main.java
+++ src/Main.java
@@ -8,20 +8,36 @@ public class Main {
     public static void main(String[] args) {
 
 
-        for (int i = 0; i < 4; i++) {
-            conditonalsList.add(Integer.toString(i));
-
-        }
+        addOneElements();
+        addTwoElements();
 
         printList(conditonalsList);
 
     }
 
 
-
     private static void printList(List<String> stringList) {
         for (String stringFromList : stringList)
             System.out.println(stringFromList);
+    }
+
+    private static void addOneElements() {
+        for (int i = 0; i < 4; i++) {
+            conditonalsList.add(Integer.toString(i));
+
+        }
+
+
+    }
+
+    private static void addTwoElements() {
+        String worldToAdd = "";
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++)
+
+                conditonalsList.add(Integer.toString(i) + ", " + j);
+
+        }
     }
 
 }
