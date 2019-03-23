5c2e2b7be6306ee82bea9b1b6a0fc7a5fb70cc58
Frank <frank.leuchtmann@gmx.de>
2019-03-23 08:32:35 +0100

    creted working sub set method

--- src/gui/PowerSet.java
+++ src/gui/PowerSet.java
@@ -4,25 +4,74 @@ import java.util.*;
 
 public class PowerSet {
     public static void main(String[] args) {
-        int set[] = new int[8];
-        for (int i = 0; i < set.length; i++) {
-            set[i] = i;
+        List<Integer> niceList = new LinkedList<>();
+        niceList.add(1);
+        niceList.add(2);
+        niceList.add(3);
+
+        //own();
+        List<List<Integer>> betterList = subSet(niceList);
+        System.out.println(betterList);
+
+    }
+
+    public static void own() {
+        List<Integer> set = new LinkedList<>();
+        for (int i = 0; i < 8; i++) {
+            set.add(i);
         }
 
         List<Set> sets = new LinkedList<>();
-        sets.add(new TreeSet());
+        //sets.add(new TreeSet());
 
         for (int currentWorld : set) {
             for (int i = 7; i > 0; i--) {
                 Set newSet = new TreeSet<>(Collections.reverseOrder());
                 newSet.add(currentWorld);
-                newSet.add(set[i]);
+                newSet.add(set);
+
                 sets.add(newSet);
             }
 
         }
 
         System.out.println(sets);
+        System.out.println(sets.size());
+    }
 
+    //    public static List<List<Integer>> createPowerSet(List<Integer> base){
+//        List<List<Integer>> listToReturn = new ArrayList<Integer>;
+//
+//        for(Integer world: base){
+//            List<Integer> newList = new ArrayList<>()
+//        }
+//
+//    }
+    public static <T> List<List<T>> powerSet(List<T> input) {
+        List<List<T>> sets = new ArrayList<>();
+        for (T element : input) {
+            for (ListIterator<List<T>> setsIterator = sets.listIterator(); setsIterator.hasNext(); ) {
+                List<T> newSet = new ArrayList<>(setsIterator.next());
+                newSet.add(element);
+                setsIterator.add(newSet);
+            }
+            sets.add(new ArrayList<>(Arrays.asList(element))); //!!!
+        }
+        sets.add(new ArrayList<>());
+        return sets;
+    }
+
+    public static  List<List<Integer>> subSet(List<Integer> input) {
+        List<List<Integer>> sets = new ArrayList<>();
+        for (Integer world : input) {
+            for (ListIterator<List<Integer>> setsIterator = sets.listIterator(); setsIterator.hasNext(); ) {
+                List<Integer> newSet = new ArrayList<>(setsIterator.next());
+                newSet.add(world);
+                setsIterator.add(newSet);
+            }
+            sets.add(new ArrayList<>(Arrays.asList(world)));
+        }
+        sets.add(new ArrayList<>());
+        return sets;
     }
 }
