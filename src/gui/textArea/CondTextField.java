00438f3cf1d50b44cbd41773bcf8eb7c3a0b4d4a
Frank <frank.leuchtmann@gmx.de>
2019-03-30 16:36:20 +0100

    improved line numbers in conditionals panel

--- src/gui/textArea/CondTextField.java
+++ src/gui/textArea/CondTextField.java
@@ -16,7 +16,7 @@ public class CondTextField extends JTextArea {
         setText("");
         int numberCounter = 0;
         for (int i = 0; i < DataContainer.getWorldsList().size(); i++) {
-            append(i + 1 + "    " + DataContainer.getWorldsList().get(i).toString() + "\n");
+            append(getLineNumber(i) + DataContainer.getWorldsList().get(i).toString() + "\n");
             numberCounter++;
 
         }
@@ -27,7 +27,7 @@ public class CondTextField extends JTextArea {
         setText("");
         int numberOfConditionals = 0;
         for (int i = 0; i < DataContainer.getConditionalSet().size(); i++) {
-            append(i + 1 + "    " + DataContainer.getConditionalSet().get(i).toString() + "\n");
+            append(getLineNumber(i) + DataContainer.getConditionalSet().get(i).toString() + "\n");
             numberOfConditionals++;
         }
         return numberOfConditionals;
@@ -38,9 +38,18 @@ public class CondTextField extends JTextArea {
         setText("");
         for (int i = 0; i < DataContainer.getCnfc().size(); i++) {
             ConditionalList currentList = DataContainer.getCnfc().get(i);
-            append(i + 1 + "   " + currentList.toString() + "\n");
+            append(getLineNumber(i) + currentList.toString() + "\n");
             numberOfNfc++;
         }
         return numberOfNfc;
+    }
+
+    public String getLineNumber(int i) {
+        String lineNumber;
+        i = i + 1;
+        if (i < 10)
+            lineNumber = "0" + i;
+        else lineNumber = Integer.toString(i);
+        return lineNumber + "   ";
     }
 }
