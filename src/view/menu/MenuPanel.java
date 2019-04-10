3985628f35f7e1dcccd074ff883a90b6319263a2
Frank <frank.leuchtmann@gmx.de>
2019-04-10 16:39:54 +0200

    improved layout in menuPanel

--- src/view/menu/MenuPanel.java
+++ src/view/menu/MenuPanel.java
@@ -4,17 +4,18 @@ import controller.GuiObserver;
 
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.HashMap;
 
 public class MenuPanel extends JPanel {
     private OptionsPanel optionsPanel;
 
     public MenuPanel(GuiObserver observer) {
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        setLayout(new BorderLayout());
 
         optionsPanel = new OptionsPanel();
-        add(optionsPanel);
-        add(new StartPanel(observer));
+        add(optionsPanel, BorderLayout.CENTER);
+        add(new StartPanel(observer), BorderLayout.SOUTH);
     }
 
     public HashMap<String, String> getOptions() {
