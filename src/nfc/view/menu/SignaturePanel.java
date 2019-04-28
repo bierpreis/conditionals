0ae455b76a18bb918357dacc8fc23cd719ef5180
Frank <frank.leuchtmann@gmx.de>
2019-04-28 15:31:50 +0200

    simplified signature panel

--- src/nfc/view/menu/SignaturePanel.java
+++ src/nfc/view/menu/SignaturePanel.java
@@ -1,28 +1,20 @@
 package nfc.view.menu;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.beans.PropertyChangeSupport;
 
 public class SignaturePanel extends JPanel {
 
     private ButtonGroup signatureButtonGroup;
-    private JRadioButton abRadioButton;
-
-    private PropertyChangeSupport changes = new PropertyChangeSupport(this);
 
     public SignaturePanel() {
         setBorder(BorderFactory.createTitledBorder("Choose Signature"));
 
         signatureButtonGroup = new ButtonGroup();
 
-        abRadioButton = new JRadioButton("ab");
+        JRadioButton abRadioButton = new JRadioButton("ab");
         JRadioButton abcRadioButton = new JRadioButton("abc");
 
-        abRadioButton.addActionListener(new SignatureRadioButtonListener());
         abRadioButton.setActionCommand("ab");
-        abcRadioButton.addActionListener(new SignatureRadioButtonListener());
         abcRadioButton.setActionCommand("abc");
 
         add(abRadioButton);
@@ -35,14 +27,6 @@ public class SignaturePanel extends JPanel {
 
         setBorder(BorderFactory.createTitledBorder("Signature"));
 
-    }
-
-    //todo: what is this
-    class SignatureRadioButtonListener implements ActionListener {
-
-        public void actionPerformed(ActionEvent e) {
-            changes.firePropertyChange("signature:" + signatureButtonGroup.getSelection().getActionCommand(), true, false);
-        }
     }
 
     public String getRequestedSignature() {
