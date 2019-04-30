9f31d1f5fca9815f5edc8ee5d63c6389faca446c
Frank <frank.leuchtmann@gmx.de>
2019-04-30 10:32:56 +0200

    finaly found the right button, but not implemented yet

--- src/kb_creator/gui/leftpanel/actionpanel/StopButton.java
+++ src/kb_creator/gui/leftpanel/actionpanel/StopButton.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Locale;
 
 
 public class StopButton extends JButton {
@@ -19,80 +20,14 @@ public class StopButton extends JButton {
         @Override
         public void actionPerformed(ActionEvent e) {
 
-            StopWarning stopWarning = new StopWarning();
-            System.out.println("answer: " + stopWarning.askIfStop());
+            UIManager.put("OptionPane.yesButtonText", "Yes");
+            UIManager.put("OptionPane.noButtonText", "No");
+
+            JOptionPane.setDefaultLocale(Locale.UK);
+            int input = JOptionPane.showConfirmDialog(null, "Do you like bacon?", "lol",  JOptionPane.YES_NO_OPTION);
+            System.out.println("input: " + input);
         }
     }
 }
 
-
-class StopWarning {
-    JButton stopButton;
-    JButton continueButton;
-
-    String buttonAnswer;
-
-    StopWarning() {
-
-
-    }
-
-    public boolean askIfStop() {
-
-
-        JDialog dialog = new JDialog();
-        dialog.setLayout(new BorderLayout());
-        dialog.setTitle("Warning");
-
-
-        JPanel buttonPanel = new JPanel();
-
-
-        ButtonGroup buttonGroup = new ButtonGroup();
-        stopButton = new JButton("Stop");
-        stopButton.setBounds(0, 50, 100, 50);
-        stopButton.addActionListener(new DialogListener());
-        buttonGroup.add(stopButton);
-        buttonPanel.add(stopButton);
-
-        continueButton = new JButton("Continue");
-        continueButton.setBounds(100, 50, 100, 50);
-        continueButton.addActionListener(new DialogListener());
-        dialog.getRootPane().setDefaultButton(continueButton);
-        buttonGroup.add(continueButton);
-        buttonPanel.add(continueButton);
-
-        dialog.add(buttonPanel, BorderLayout.SOUTH);
-
-        JPanel questionPanel = new JPanel();
-        questionPanel.setLayout(new FlowLayout());
-        questionPanel.add(new JLabel("Are you sure you want to stop creating KBs?"));
-
-        dialog.add(questionPanel);
-        dialog.revalidate();
-        dialog.pack();
-        dialog.setVisible(true);
-
-        int reply = JOptionPane.showConfirmDialog(null, "message", "title", JOptionPane.YES_NO_OPTION);
-        if (reply == JOptionPane.YES_OPTION) {
-            JOptionPane.showMessageDialog(null, "HELLO");
-            return true;
-        }
-        else {
-            JOptionPane.showMessageDialog(null, "GOODBYE");
-            return false;
-        }
-    }
-
-    private class DialogListener implements ActionListener {
-
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-
-            buttonAnswer = e.getActionCommand();
-
-        }
-    }
-}
 
