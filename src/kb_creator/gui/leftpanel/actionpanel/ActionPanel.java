3168a820fac8f4013a588ad263ac13c83e4e616e
Frank <frank.leuchtmann@gmx.de>
2019-05-01 18:10:51 +0200

    added todo

--- src/kb_creator/gui/leftpanel/actionpanel/ActionPanel.java
+++ src/kb_creator/gui/leftpanel/actionpanel/ActionPanel.java
@@ -21,7 +21,7 @@ public class ActionPanel extends JPanel {
         pauseButton = new PauseButton();
         pauseButton.addActionListener(observer);
         add(pauseButton);
-
+        //todo: stop does not really work
         stopButton = new StopButton();
         stopButton.addActionListener(observer);
         add(stopButton);
