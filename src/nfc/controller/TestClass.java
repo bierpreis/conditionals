26e2ca96f165e43319054254d15903b428874df2
Frank <frank.leuchtmann@gmx.de>
2019-05-10 12:07:37 +0200

    implemented test in testclass for double negation

--- src/nfc/controller/TestClass.java
+++ src/nfc/controller/TestClass.java
@@ -1,5 +1,6 @@
 package nfc.controller;
 
+import com.google.errorprone.annotations.Var;
 import kb_creator.model.PropositionalLogic.AbstractFormula;
 import kb_creator.model.PropositionalLogic.Atom;
 import kb_creator.model.PropositionalLogic.Variable;
@@ -11,9 +12,13 @@ public class TestClass {
 
         AbstractFormula a = new Atom(Variable.a);
         AbstractFormula notA = new Atom(Variable.a).neg();
+        AbstractFormula notNotA = new Atom(Variable.a);
+        notNotA = notNotA.neg();
+        notNotA = notNotA.neg();
 
         System.out.println("a: " + a);
         System.out.println("b: " + notA);
+        System.out.println("notnot: " + notNotA);
     }
 
 
