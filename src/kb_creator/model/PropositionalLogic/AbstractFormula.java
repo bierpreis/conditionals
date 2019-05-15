0985a10272d39ee38e8d37fcbaf2216f3b38203d
Frank <frank.leuchtmann@gmx.de>
2019-05-15 11:07:26 +0200

    implemented equals method for negation

--- src/kb_creator/model/PropositionalLogic/AbstractFormula.java
+++ src/kb_creator/model/PropositionalLogic/AbstractFormula.java
@@ -7,6 +7,7 @@ public abstract class AbstractFormula {
     public AbstractFormula and(AbstractFormula otherFormula) {
         return new Conjunction(this, otherFormula);
     }
+
     //todo: why is this never used?
     public AbstractFormula or(AbstractFormula otherFormula) {
         return new Disjunction(this, otherFormula);
@@ -18,5 +19,8 @@ public abstract class AbstractFormula {
     }
 
     public abstract String toString();
+
+    @Override
+    public abstract boolean equals(Object o);
 
 }
--- src/kb_creator/model/PropositionalLogic/Negation.java
+++ src/kb_creator/model/PropositionalLogic/Negation.java
@@ -2,7 +2,7 @@ package kb_creator.model.PropositionalLogic;
 
 public class Negation extends AbstractFormula {
     private AbstractFormula formula;
-    
+
     public Negation(AbstractFormula formula) {
         this.formula = formula;
     }
@@ -18,6 +18,18 @@ public class Negation extends AbstractFormula {
 
     @Override
     public AbstractFormula neg() {
+        return formula;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Negation))
+            return false;
+        Negation otherNegation = (Negation) o;
+        return otherNegation.getFormula().equals(formula);
+    }
+
+    public AbstractFormula getFormula() {
         return formula;
     }
 }
