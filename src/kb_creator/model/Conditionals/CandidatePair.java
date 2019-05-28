9332ca4e2e464d0d0d3c16287c8961b453ac1363
Frank <frank.leuchtmann@gmx.de>
2019-05-28 05:26:54 +0200

    building memory efficient stuff

--- src/kb_creator/model/Conditionals/CandidatePair.java
+++ src/kb_creator/model/Conditionals/CandidatePair.java
@@ -1,17 +1,35 @@
 package kb_creator.model.Conditionals;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+
 //todo: here conditionals map
 public class CandidatePair {
     private KnowledgeBase knowledgeBase;
-    private List<NewConditional> candidates;
+    private List<Integer> candidates;
+
+    private Map<Integer, NewConditional> conditionalMap;
 
     public CandidatePair(KnowledgeBase knowledgeBase, List<NewConditional> candidates) {
         this.knowledgeBase = knowledgeBase;
-        this.candidates = candidates;
+
+        for (NewConditional conditionalToAdd : candidates) {
+            this.candidates.add(conditionalToAdd.getNumber());
+        }
     }
 
+    //todo: only use this when really needed. maybe delete?
     public List<NewConditional> getCandidates() {
+        List<NewConditional> candidatesList = new LinkedList<>();
+        for (Integer candidateNumber : candidates) {
+            candidatesList.add(conditionalMap.get(candidateNumber));
+        }
+
+        return candidatesList;
+    }
+
+    public List<Integer> getCandidatesNumbers() {
         return candidates;
     }
 
@@ -29,5 +47,9 @@ public class CandidatePair {
 
     public void deleteKB() {
         knowledgeBase = null;
+    }
+
+    public void setMap(Map<Integer, NewConditional> conditionalMap) {
+        this.conditionalMap = conditionalMap;
     }
 }
