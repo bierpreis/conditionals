acc6544e2b3f1ad7e278148e8d1f5e07050413d3
Frank <frank.leuchtmann@gmx.de>
2019-05-29 06:26:17 +0200

    removed todo

--- src/kb_creator/model/Conditionals/KBCreator.java
+++ src/kb_creator/model/Conditionals/KBCreator.java
@@ -44,7 +44,7 @@ public class KBCreator implements Runnable {
         //k in original paper starts at 1
         //here it starts at 0 because lists in java start at 0 and not 1
         k = 0;
-        //todo: here conditionals map
+
         List<List<CandidatePair>> l = new ArrayList<>();
 
         List<NewConditional> nfc = nfcCreator.getNewNfc();
