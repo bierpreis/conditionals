7651029cae768c72b4265521a87a2ee19bac1916
Frank <frank.leuchtmann@gmx.de>
2019-06-03 10:19:00 +0200

    simpilified gui

--- src/kb_creator/gui/leftpanel/optionsPanel/BufferPanel/BufferCheckboxPanel.java
+++ src/kb_creator/gui/leftpanel/optionsPanel/BufferPanel/BufferCheckboxPanel.java
@@ -2,8 +2,6 @@ package kb_creator.gui.leftpanel.optionsPanel.BufferPanel;
 
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class BufferCheckboxPanel extends JPanel {
     private JCheckBox saveCheckBox;
@@ -13,19 +11,10 @@ public class BufferCheckboxPanel extends JPanel {
 
         saveCheckBox = new JCheckBox("Buffer Files to Disk");
         add(saveCheckBox);
-        saveCheckBox.addActionListener(new BufferCheckBoxActionListener());
 
         saveCheckBox.setEnabled(false);
     }
 
-    class BufferCheckBoxActionListener implements ActionListener {
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            //todo: delete or do sth?
-            ;
-        }
-    }
 
     public boolean isSelected() {
         return saveCheckBox.isSelected();
