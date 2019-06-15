1e4f5e543f3b1e53fc573513d13a853518c06da0
Frank <frank.leuchtmann@gmx.de>
2019-06-15 13:39:54 +0200

    added string EMPTY to cp file when there are no candidates

--- src/kb_creator/model/Conditionals/Pairs/CandidateNumbersArrayPair.java
+++ src/kb_creator/model/Conditionals/Pairs/CandidateNumbersArrayPair.java
@@ -27,22 +27,21 @@ public class CandidateNumbersArrayPair extends AbstractPair {
 
     public CandidateNumbersArrayPair(String stringFromFile) {
 
-        String[] splitString = stringFromFile.split("candidates");
+        String[] splitString = stringFromFile.split("candidates\n");
         if (splitString.length != 2)
             throw new RuntimeException("Invalid Candidate Pair File: " + splitString.length + "length");
 
         //divide string into kb and candidates
         //create both from subString
         knowledgeBase = new ObjectKnowledgeBase(splitString[0]);
-        if (splitString[1] == "")
-            System.out.println("wtf");
+
         candidatesNumbersArray = createCandidatesArrayFromString(splitString[1]);
     }
 
 
     private int[] createCandidatesArrayFromString(String inputString) {
         if (inputString.length() < 2)
-            System.out.println(inputString); //todo: input string can be "\n", then it fucks up.
+            System.out.println(inputString);
         inputString = inputString.replaceAll("\n", "");
 
         String[] candidatesStringArray = inputString.split(", ");
@@ -50,10 +49,12 @@ public class CandidateNumbersArrayPair extends AbstractPair {
         for (String candidateString : candidatesStringArray) {
             candidateString.replace(", ", "");
         }
-        for (int i = 0; i < candidatesStringArray.length; i++) {
-            if (candidatesStringArray[i].length() != 0)
-                candidatesArray[i] = Integer.parseInt(candidatesStringArray[i]);
-        }
+        //it will be "EMPTY" if there are no candidates
+        if (!inputString.equals("EMPTY"))
+            for (int i = 0; i < candidatesStringArray.length; i++) {
+                if (candidatesStringArray[i].length() != 0)
+                    candidatesArray[i] = Integer.parseInt(candidatesStringArray[i]);
+            }
         return candidatesArray;
     }
 
@@ -63,13 +64,13 @@ public class CandidateNumbersArrayPair extends AbstractPair {
         List<NewConditional> candidatesList = new ArrayList<>();
 
         for (int candidateNumber : this.candidatesNumbersArray) {
-            //todo: number can be 0 which is shit!
+
             if (candidateNumber == 0)
                 System.out.println("number was 0!!");
             candidatesList.add(nfcMap.get(candidateNumber));
         }
 
-        //todo: this list can contain null
+
         if (candidatesList.contains(null))
             System.out.println("null in there!");
         return candidatesList;
@@ -98,12 +99,13 @@ public class CandidateNumbersArrayPair extends AbstractPair {
         sb.append(knowledgeBase.toShortFileString());
         sb.append("\n\n");
         sb.append("candidates\n");
-
-        for (int i = 0; i < candidatesNumbersArray.length; i++) {
-            sb.append(candidatesNumbersArray[i]);
-            if (i != candidatesNumbersArray.length - 1)
-                sb.append(", ");
-        }
+        if (candidatesNumbersArray.length > 0) {
+            for (int i = 0; i < candidatesNumbersArray.length; i++) {
+                sb.append(candidatesNumbersArray[i]);
+                if (i != candidatesNumbersArray.length - 1)
+                    sb.append(", ");
+            }
+        } else sb.append("EMPTY");
         return sb.toString();
     }
 
