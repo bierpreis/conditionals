38ddfd573d243825d5ff82251ac92766cb4ad2e0
Frank <frank.leuchtmann@gmx.de>
2019-06-18 12:12:17 +0200

    updated todo

--- src/kb_creator/gui/writerPanel/MainWriterPanel.java
+++ src/kb_creator/gui/writerPanel/MainWriterPanel.java
@@ -2,6 +2,7 @@ package kb_creator.gui.writerPanel;
 
 import kb_creator.gui.writerPanel.KbPanel.MainKbWriterPanel;
 
+//todo: delete or implement
 public class MainWriterPanel {
     private MainKbWriterPanel kbPanel;
     private MainCandidatesPanel mainCandidatesPanel;
