111004e2b90c5da7b1de5528e03449062ded1c04
Frank <frank.leuchtmann@gmx.de>
2019-06-19 06:02:32 +0200

    fixed bug in compressed array pair

--- src/kb_creator/model/Conditionals/Pairs/CompressedCandidateArrayPair.java
+++ src/kb_creator/model/Conditionals/Pairs/CompressedCandidateArrayPair.java
@@ -1,6 +1,5 @@
 package kb_creator.model.Conditionals.Pairs;
 
-import com.intellij.refactoring.changeClassSignature.TypeParameterInfo;
 import kb_creator.model.Conditionals.KnowledgeBase.AbstractKnowledgeBase;
 import kb_creator.model.Conditionals.NewConditional;
 
@@ -14,21 +13,21 @@ public class CompressedCandidateArrayPair extends AbstractPair {
     public CompressedCandidateArrayPair(AbstractKnowledgeBase knowledgeBase, List<NewConditional> candidates) {
         this.knowledgeBase = knowledgeBase;
 
-        compressedCandidatesArray = new int[8][2];
+        compressedCandidatesArray = new int[5][2];
 
         int lastConditionalNumber = 0;
-        int arrayNumber = 0;
-
+        int nextArrayNumber = 0;
 
 
         for (NewConditional conditional : candidates) {
             if (conditional.getNumber() != lastConditionalNumber + 1) {
-                compressedCandidatesArray[arrayNumber][0] = conditional.getNumber();
-                compressedCandidatesArray[arrayNumber][1] = conditional.getNumber();
+                compressedCandidatesArray[nextArrayNumber][0] = conditional.getNumber();
+                compressedCandidatesArray[nextArrayNumber][1] = conditional.getNumber();
 
+                nextArrayNumber = nextArrayNumber + 1;
                 lastConditionalNumber = conditional.getNumber();
             } else {
-                compressedCandidatesArray[arrayNumber][1] = conditional.getNumber();
+                compressedCandidatesArray[nextArrayNumber-1][1] = conditional.getNumber();
                 lastConditionalNumber++;
             }
 
@@ -42,8 +41,9 @@ public class CompressedCandidateArrayPair extends AbstractPair {
         List<NewConditional> candidatesList = new ArrayList<>();
         for (int[] candidateBounds : compressedCandidatesArray) {
             //if it is 0, all candidates are found
-            if (candidateBounds[0] == 0)
+            if (candidateBounds[0] == 0) {
                 return candidatesList;
+            }
             for (int i = candidateBounds[0]; i <= candidateBounds[1]; i++) {
 
                 candidatesList.add(nfcMap.get(i));
