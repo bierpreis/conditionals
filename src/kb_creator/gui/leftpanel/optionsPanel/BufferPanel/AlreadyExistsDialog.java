de22f6df9a1f40f59509a66fa381f153d1cc83ef
Frank <frank.leuchtmann@gmx.de>
2019-06-23 22:41:51 +0200

    added close function to warning dialog

--- src/kb_creator/gui/leftpanel/optionsPanel/BufferPanel/AlreadyExistsDialog.java
+++ src/kb_creator/gui/leftpanel/optionsPanel/BufferPanel/AlreadyExistsDialog.java
@@ -1,13 +1,29 @@
 package kb_creator.gui.leftpanel.optionsPanel.BufferPanel;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class AlreadyExistsDialog {
-    AlreadyExistsDialog(String pathOfExistingFolder){
+    AlreadyExistsDialog(String pathOfExistingFolder) {
         JDialog warningDialog = new JDialog();
+        warningDialog.setLayout(new BoxLayout(warningDialog.getContentPane(), BoxLayout.PAGE_AXIS));
         warningDialog.add(new JLabel("The Folder you chosse already exits."));
+        warningDialog.setPreferredSize(new Dimension(350, 150));
         warningDialog.add(new JLabel("Choose diffrent Folder or delete it go continue"));
-        warningDialog.pack();
+
+        JButton okButton = new JButton("Ok");
+        warningDialog.add(okButton);
+
+        okButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)
+            {
+                warningDialog.dispose();
+            }
+        });
+
+        warningDialog.setModal(true);
         warningDialog.setVisible(true);
     }
 }
