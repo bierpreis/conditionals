ff66ae363576c6d5c251ca2c7bf3f0bd914fd574
Frank <frank.leuchtmann@gmx.de>
2019-06-24 05:57:42 +0200

    improved view of warning dialog

--- src/kb_creator/gui/leftpanel/optionsPanel/BufferPanel/AlreadyExistsDialog.java
+++ src/kb_creator/gui/leftpanel/optionsPanel/BufferPanel/AlreadyExistsDialog.java
@@ -5,18 +5,24 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-//todo: improve view
 public class AlreadyExistsDialog {
     AlreadyExistsDialog(String pathOfExistingFolder) {
         JDialog warningDialog = new JDialog();
+        warningDialog.add(new JLabel(" "));
         warningDialog.setLayout(new BoxLayout(warningDialog.getContentPane(), BoxLayout.PAGE_AXIS));
         warningDialog.add(new JLabel("The Folder you chosse already exits."));
         warningDialog.setPreferredSize(new Dimension(350, 150));
         warningDialog.add(new JLabel("Choose diffrent Folder or delete it go continue"));
+        warningDialog.add(new JLabel(""));
         warningDialog.add(new JLabel(pathOfExistingFolder));
 
+        //jpanel is used for centering button
+        JPanel buttonPanel = new JPanel();
         JButton okButton = new JButton("Ok");
-        warningDialog.add(okButton);
+        warningDialog.add(Box.createHorizontalGlue());
+        buttonPanel.add(okButton);
+        warningDialog.add(buttonPanel);
+        warningDialog.add(Box.createHorizontalGlue());
 
         okButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
