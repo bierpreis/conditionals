fd8ed9dbd30424de06e59f92a15297498569c330
Frank <frank.leuchtmann@gmx.de>
2019-07-01 05:59:05 +0200

    more paraleell buffer

--- src/kb_creator/model/conditionals/pair_lists/ParallelBufferedList.java
+++ src/kb_creator/model/conditionals/pair_lists/ParallelBufferedList.java
@@ -15,10 +15,14 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class ParallelBufferedList extends AbstractCandidateCollection {
     private Queue queueToReturn;
     private Queue queueToPrepare;
+    private List<String> fileStringList;
+    private int currentK;
+    private int nextFileToReadNumber;
 
     public ParallelBufferedList(String filePath) {
         super(filePath);
         System.out.println("created parallel list for candidate pairs");
+        currentK = 1;
 
         writeCounter = 0;
         readCounter = 0;
@@ -30,10 +34,10 @@ public class ParallelBufferedList extends AbstractCandidateCollection {
         running = true;
 
 
-            this.filePath = filePath + "/tmp/";
+        this.filePath = filePath + "/tmp/";
 
-            File tmpFile = new File(this.filePath);
-            tmpFile.mkdirs();
+        File tmpFile = new File(this.filePath);
+        tmpFile.mkdirs();
 
 
         cpQueueToWrite = new ConcurrentLinkedQueue<AbstractPair>();
@@ -43,6 +47,8 @@ public class ParallelBufferedList extends AbstractCandidateCollection {
 
         status = BufferStatus.NOT_STARTED;
 
+        fileStringList = getPairStringList(currentK);
+        nextFileToReadNumber = 0;
     }
 
     //todo
@@ -52,7 +58,7 @@ public class ParallelBufferedList extends AbstractCandidateCollection {
             if (cpQueueToWrite.size() > maxNumberOfPairsInFile || flushRequested) {
                 status = BufferStatus.WRITING;
 
-                writeAllPairs(cpQueueToWrite);
+                writeNextFile(cpQueueToWrite);
                 flushRequested = false;
             } else if (requestedKList.get() != 0) {
                 status = BufferStatus.READING;
@@ -70,9 +76,8 @@ public class ParallelBufferedList extends AbstractCandidateCollection {
 
 
     }
-
-    //todo
-    private void writeAllPairs(Queue queueToWrite) {
+    
+    private void writeNextFile(Queue queueToWrite) {
         File subFolder = null;
         if (!queueToWrite.isEmpty()) {
             subFolder = new File(filePath + "/" + ((AbstractPair) queueToWrite.peek()).getKnowledgeBase().getSize() + "/");
@@ -82,27 +87,25 @@ public class ParallelBufferedList extends AbstractCandidateCollection {
 
         try {
 
-            while (!queueToWrite.isEmpty()) {
-                //add leading zeros so the files will be soreted in correct order in their folder
-                String fileName = String.format("%05d", writeCounter);
-                writeCounter++;
-                PrintWriter writer = new PrintWriter(subFolder.toString() + "/" + fileName + ".txt", "UTF-8");
+            //add leading zeros so the files will be soreted in correct order in their folder
+            String fileName = String.format("%05d", writeCounter);
+            writeCounter++;
+            PrintWriter writer = new PrintWriter(subFolder.toString() + "/" + fileName + ".txt", "UTF-8");
 
-                StringBuilder sb = new StringBuilder();
+            StringBuilder sb = new StringBuilder();
 
-                for (int i = 0; i < maxNumberOfPairsInFile && !queueToWrite.isEmpty(); i++) {
+            for (int i = 0; i < maxNumberOfPairsInFile && !queueToWrite.isEmpty(); i++) {
 
-                    AbstractPair pairToWrite = (AbstractPair) queueToWrite.poll();
-                    sb.append(pairToWrite.toFileString());
-                    sb.append("\nEND_PAIR\n\n");
-                    pairToWrite.deleteCandidates();
-                    pairToWrite.deleteKB();
+                AbstractPair pairToWrite = (AbstractPair) queueToWrite.poll();
+                sb.append(pairToWrite.toFileString());
+                sb.append("\nEND_PAIR\n\n");
+                pairToWrite.deleteCandidates();
+                pairToWrite.deleteKB();
 
-                }
-
-                writer.print(sb.toString().replaceAll("\nEND_PAIR\n\n$", ""));
-                writer.close();
             }
+
+            writer.print(sb.toString().replaceAll("\nEND_PAIR\n\n$", ""));
+            writer.close();
 
 
         } catch (IOException e) {
@@ -111,7 +114,7 @@ public class ParallelBufferedList extends AbstractCandidateCollection {
 
     }
 
-    //todo
+    //todo: read next file
     private Collection<AbstractPair> readPairs(int requestedK) {
         requestedKList.set(requestedK);
         while (!requestedListIsReady) {
@@ -160,10 +163,11 @@ public class ParallelBufferedList extends AbstractCandidateCollection {
         for (String fileString : fileStringList) {
             pairStringList.addAll(Arrays.asList(fileString.split("\nEND_PAIR\n\n")));
         }
+        currentK++;
         return pairStringList;
     }
 
-    //todo
+    //todo: read next file
     private List<AbstractPair> readAllPairs(int requestedK) {
         readCounter = 0;
         List<String> stringList = getPairStringList(requestedK);
