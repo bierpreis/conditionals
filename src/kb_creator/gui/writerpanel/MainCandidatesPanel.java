4ca568b6770d916e6c506fc13786d9c3c9fcd6b3
Frank <frank.leuchtmann@gmx.de>
2019-07-04 17:29:19 +0200

    added todos

--- src/kb_creator/gui/writerpanel/MainCandidatesPanel.java
+++ src/kb_creator/gui/writerpanel/MainCandidatesPanel.java
@@ -31,15 +31,20 @@ public class MainCandidatesPanel extends JPanel {
 
     }
 
+    //todo: useless. almost always shows reading as status. is this correct? if yes, remove
     public void showStatus(AbstractPairBuffer.BufferStatus status) {
         statusLabel.setText("Status: " + status.toString());
     }
 
+
+    //todo: this is useless. at least write "writer queue" or remove completely.
     public void showWriterQueue(int alreadyFinishedNumber) {
         NumberFormat formatter = NumberFormat.getInstance(new Locale("de_DE"));
         writerLabel.setText("Queue: " + formatter.format(alreadyFinishedNumber));
     }
 
+
+    //todo: this is some useless information with parallel buffer. ony useful with simple buffer? what does it mean. does it make sense? write it in description or delete info
     public void showReaderProgress(int alreadyReadNumber) {
         NumberFormat formatter = NumberFormat.getInstance(new Locale("de_DE"));
         readerLabel.setText("Read KBs: " + formatter.format(alreadyReadNumber));
