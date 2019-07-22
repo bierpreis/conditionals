89acacfe63e5ed4d056eca440c0f44e84a5d7341
Frank <frank.leuchtmann@gmx.de>
2019-07-22 17:18:52 +0200

    added todo

--- src/kb_creator/model/conditionals/KBCreator.java
+++ src/kb_creator/model/conditionals/KBCreator.java
@@ -275,6 +275,7 @@ public class KBCreator implements Runnable {
     }
 
     private Map<Integer, NewConditional> createNfcMap(Collection<NewConditional> nfc) {
+        //todo: read about load factor for hashmap tuning
         Map<Integer, NewConditional> conditionalMap = new HashMap<>(nfc.size());
         for (NewConditional conditional : nfc) {
             if (conditionalMap.containsKey(conditional.getNumber())) {
