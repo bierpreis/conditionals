f4d69520baa40626d9631730350e7daee5a1081a
Frank <frank.leuchtmann@gmx.de>
2019-08-08 14:53:16 +0200

    added comment

--- src/kb_creator/model/knowledge_base/ObjectKnowledgeBase.java
+++ src/kb_creator/model/knowledge_base/ObjectKnowledgeBase.java
@@ -87,6 +87,7 @@ public class ObjectKnowledgeBase extends AbstractKnowledgeBase {
 
         for (NewConditional conditionalFromList : conditionalList) {
             if (consistencyOfKB == null)
+                //this is actually the implication
                 consistencyOfKB = new Conjunction(conditionalFromList.getAntecedent().neg().or(conditionalFromList.getConsequence()));
             else
                 consistencyOfKB = consistencyOfKB.and(conditionalFromList.getAntecedent().neg().or(conditionalFromList.getConsequence()));
