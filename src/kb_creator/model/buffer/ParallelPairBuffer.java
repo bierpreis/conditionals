2f0b79b35bdb5b1680fdfd6cd066116a3c5a542d
Frank <frank.leuchtmann@gmx.de>
2019-08-10 15:47:46 +0200

    updated todo

--- src/kb_creator/model/buffer/ParallelPairBuffer.java
+++ src/kb_creator/model/buffer/ParallelPairBuffer.java
@@ -205,9 +205,8 @@ public class ParallelPairBuffer extends AbstractPairBuffer {
     public AbstractPair getNextPair(int currentK) {
         while (queueToReturn.peek() == null)
             try {
-                //todo: this happens all the time with ab and buffering ->it happens always when reading files. put more data in file?! and queue min in here.
-                System.out.println("buffer sleeping because queue is empty! this should only happen when new iteration is started.");
-                Thread.sleep(100);
+                System.out.println("buffer sleeping because queue is empty!");
+                Thread.sleep(80);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
