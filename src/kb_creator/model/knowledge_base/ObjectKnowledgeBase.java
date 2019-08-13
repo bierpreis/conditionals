d993f670ed381f852e0d797dcb6d723989f15c20
Frank <frank.leuchtmann@gmx.de>
2019-08-13 15:03:55 +0200

    updated todo

--- src/kb_creator/model/knowledge_base/ObjectKnowledgeBase.java
+++ src/kb_creator/model/knowledge_base/ObjectKnowledgeBase.java
@@ -99,7 +99,7 @@ public class ObjectKnowledgeBase extends AbstractKnowledgeBase {
         for (NewConditional conditionalFromList : conditionalList) {
             if (consistencyOfKB == null)
                 //this is actually the implication
-                //todo: why conjunction? it is or so why not disjunction?!
+                //todo: why is the first formula always conjunction and rest disjunction?! wtf
                 consistencyOfKB = new Conjunction(conditionalFromList.getAntecedent().neg().or(conditionalFromList.getConsequence()));
             else
                 consistencyOfKB = consistencyOfKB.and(conditionalFromList.getAntecedent().neg().or(conditionalFromList.getConsequence()));
