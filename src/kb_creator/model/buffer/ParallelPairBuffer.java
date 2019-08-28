6637b59299ecd552aab2d8cb85d9f741c96b68e4
Frank <frank.leuchtmann@gmx.de>
2019-08-28 14:27:37 +0200

    added todo

--- src/kb_creator/model/buffer/ParallelPairBuffer.java
+++ src/kb_creator/model/buffer/ParallelPairBuffer.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class ParallelPairBuffer extends AbstractPairBuffer {
 
--- src/kb_creator/model/knowledge_base/ObjectKnowledgeBase.java
+++ src/kb_creator/model/knowledge_base/ObjectKnowledgeBase.java
@@ -17,6 +17,7 @@ public class ObjectKnowledgeBase extends AbstractKnowledgeBase {
 
     private List<NewConditional> conditionalList;
 
+    //todo: this is shit. at least one of theese is useless. or make static?
     private final Pattern AB_PATTERN = Pattern.compile("^a,b.*");
     private final Pattern ABC_PATTERN = Pattern.compile("^a,b,c.*");
 
