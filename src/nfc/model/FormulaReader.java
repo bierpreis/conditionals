f062bcc2ff3a699cf24efe5235adb841a1df8599
Frank <frank.leuchtmann@gmx.de>
2019-09-18 09:26:42 +0200

    more formula reader

--- src/nfc/model/FormulaReader.java
+++ src/nfc/model/FormulaReader.java
@@ -1,18 +1,23 @@
 package nfc.model;
 
-import kb_creator.model.propositional_logic.AbstractFormula;
-import kb_creator.model.propositional_logic.Tautology;
+import kb_creator.model.propositional_logic.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FormulaReader {
+    static Pattern worldPattern = Pattern.compile("!?[abc]{1}");
 
 
     public static void main(String[] args) {
+        String test = "!abc";
+        System.out.println("formula: " + getNextAtom(test));
+/*
         String filePath = "/home/bierpreis/test";
         FormulaReader formulaReader = new FormulaReader();
 
@@ -22,6 +27,41 @@ public class FormulaReader {
             System.out.println(formula);
 
 
+Matcher matcher = worldPattern.matcher(test);
+        System.out.println(matcher.matches());
+*/
+
+        String output = "";
+        if (test.matches("^!.*"))
+            output += "negation";
+        test = test.replaceFirst("^!?[abc]{1}", "");
+        System.out.println("output: " + output);
+        System.out.println("test: " + test);
+    }
+
+    private static AbstractFormula getNextAtom(String string) {
+        AbstractFormula formulaToReturn = null;
+        if (string.matches("^!.*")) {
+            string = string.replaceFirst("^!", "");
+
+            if (string.matches("^a.*"))
+                formulaToReturn = new Negation(new Atom(Var.a));
+            else if (string.matches("^b.*"))
+                formulaToReturn = new Negation(new Atom(Var.b));
+            if (string.matches("^c.*"))
+                formulaToReturn = new Negation(new Atom(Var.c));
+            return formulaToReturn;
+        } else {
+
+            if (string.matches("^a.*"))
+                formulaToReturn = new Atom(Var.a);
+            else if (string.matches("^b.*"))
+                formulaToReturn = new Atom(Var.b);
+            if (string.matches("^c.*"))
+                formulaToReturn = new Atom(Var.c);
+            
+            return formulaToReturn;
+        }
     }
 
     public List<AbstractFormula> getFormulaListFromFile(String filePath) {
