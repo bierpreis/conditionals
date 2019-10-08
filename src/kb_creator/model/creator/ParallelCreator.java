c2471b080abe02d15e97cd85516934b397bef723
Frank <frank.leuchtmann@gmx.de>
2019-10-08 11:31:57 +0200

    updated todo

--- src/kb_creator/model/creator/ParallelCreator.java
+++ src/kb_creator/model/creator/ParallelCreator.java
@@ -70,7 +70,7 @@ public class ParallelCreator extends AbstractCreator {
             //this is line 8
             while (l.hasMoreElements(k)) {
 
-                progress = calculateProgress(currentiterationPairCounter, lastIterationAmount);
+                progress = calculateProgress(currentiterationPairCounter, lastIterationAmount); //todo: currentIerationPairCounter is instantly queue max size. thats why progress starts at 30%
 
                 currentiterationPairCounter = inputQueueThread.getCounter();
 
