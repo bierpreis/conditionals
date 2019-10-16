fa1d165bbb157fe4586d5faf391b6f6c2dfd6a67
Frank <frank.leuchtmann@gmx.de>
2019-10-16 05:38:27 +0200

    updaded todo

--- src/kb_creator/model/buffer/AbstractPairBuffer.java
+++ src/kb_creator/model/buffer/AbstractPairBuffer.java
@@ -72,7 +72,7 @@ public abstract class AbstractPairBuffer implements Runnable {
         status = BufferStatus.STOPPED;
     }
 
-    //todo: think about status. if there are 2 threads, writing and reading are useless!
+    //todo: change status if this has 2 threads. reading and writing can be deleted then
     public enum BufferStatus {
         WRITING, READING, NOT_STARTED, SLEEPING, FINISHING_ITERATION, PREPARING_NEXT_ITERATION, FINISHED, STOPPED
     }
