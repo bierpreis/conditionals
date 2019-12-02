5ea3440b6b6fbe162724ede32eff294da9ceb721
Frank <frank.leuchtmann@gmx.de>
2019-12-02 06:01:48 +0100

    upaded todo

--- src/kb_creator/gui/left_panel/optionsPanel/buffer_options_panel/BufferSizePanel.java
+++ src/kb_creator/gui/left_panel/optionsPanel/buffer_options_panel/BufferSizePanel.java
@@ -14,7 +14,7 @@ public class BufferSizePanel extends JPanel {
 
     public BufferSizePanel() {
         descriptionLabel = new JLabel("Number of Candidates in File: ");
-        bufferSizeField = new JTextField("2000"); //todo: higher value
+        bufferSizeField = new JTextField("20000");  //todo: set by gui? experiment
         bufferSizeField.setPreferredSize(new Dimension(64, 16));
         add(descriptionLabel);
         add(bufferSizeField);
@@ -26,7 +26,7 @@ public class BufferSizePanel extends JPanel {
                     Integer.parseInt(bufferSizeField.getText());
 
                 } catch (NumberFormatException e) {
-                    new SizeWarningDialog(); //todo: warning with too high value
+                    new SizeWarningDialog();
                 }
             }
         });
