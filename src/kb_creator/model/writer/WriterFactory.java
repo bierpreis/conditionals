d3d651a533bb8bf2a34cd61d5e2295bcc912e926
Frank <frank.leuchtmann@gmx.de>
2019-12-18 19:55:00 +0100

    added comment

--- src/kb_creator/model/writer/WriterFactory.java
+++ src/kb_creator/model/writer/WriterFactory.java
@@ -5,6 +5,8 @@ import kb_creator.model.writer.real.KbFileWriter;
 
 public class WriterFactory {
 
+    // the reason for this class is that the writer type is in writer options
+    // so cant instantiate writer of unknown type
     public static AbstractKbWriter getKbWriter(KbWriterOptions writerOptions){
     if(writerOptions.isRealWriter())
         return new KbFileWriter(writerOptions);
