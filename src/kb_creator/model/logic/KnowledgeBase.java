39650c93e0cb19d8d7d4b17cd3ce41f77a92c6a3
Frank <frank.leuchtmann@gmx.de>
2019-12-20 11:05:43 +0100

    updated todo

--- src/kb_creator/model/logic/KnowledgeBase.java
+++ src/kb_creator/model/logic/KnowledgeBase.java
@@ -36,14 +36,16 @@ public class KnowledgeBase {
         this.conditionalList.add(conditional);
     }
 
-    //todo: makes the order a diffrence? test with consistency test
     //this is used for all but first iteration
     public KnowledgeBase(int number, KnowledgeBase knowledgeBase, PConditional conditionalToAdd) {
         this.number = number;
 
         this.conditionalList = new ArrayList<>(knowledgeBase.getConditionalList().size() + 1);
+
         conditionalList.addAll(knowledgeBase.getConditionalList());
         conditionalList.add(conditionalToAdd);
+
+        //todo: test collections unmodifyable?
     }
 
     //this constructor takes almost no time
