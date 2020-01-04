14ab5e41068e77143d1f5ce5596d008b33cc4a55
Frank <frank.leuchtmann@gmx.de>
2020-01-04 15:02:51 +0100

    muted short translations map

--- src/nfc_creator/model/ShortTranslationMap.java
+++ src/nfc_creator/model/ShortTranslationMap.java
@@ -8,10 +8,13 @@ import java.util.Map;
 public class ShortTranslationMap {
     private Map<Integer, AbstractFormula> translationMap;
 
+    //this class is muted
+    //because it causes problems in the jar file
+    //because it cant read correctly the text files in resources
     public ShortTranslationMap(AbstractSignature signature) {
 
-        FormulaReader formulaReader = new FormulaReader();
-        translationMap = formulaReader.getFormulaMapFromFile(signature);
+/*        FormulaReader formulaReader = new FormulaReader();
+        translationMap = formulaReader.getFormulaMapFromFile(signature);*/
 
     }
 
@@ -19,7 +22,7 @@ public class ShortTranslationMap {
     //problem with short formulas: printing conditionals gets out short formulas not normal form!
     public AbstractFormula translate(int worldsNumber) {
         //return null to test what happens if nothing gets translated
-        //return null;
-        return translationMap.get(worldsNumber);
+        return null;
+        //return translationMap.get(worldsNumber);
     }
 }
